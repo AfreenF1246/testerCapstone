138ed5fe3e80c5cb406d4d12b45354b38188160e
AfreenF1246 <af894@rutgers.edu>
2024-03-20 14:55:57 -0400

    metrics page

---
+++ app/src/main/java/com/example/testercapstone/metrics.java
@@ -0,0 +1,15 @@
+package com.example.testercapstone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class metrics extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_metrics);
+    }
+}
