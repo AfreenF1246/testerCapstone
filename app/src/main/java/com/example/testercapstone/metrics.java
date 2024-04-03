09eda0ce7d8737dac065ae2965f18d8f299b2550
NeDias2021 <dias.nelusha@gmail.com>
2024-04-02 22:42:38 -0400

    navigation bar completed... I think... still need to test it

--- app/src/main/java/com/example/testercapstone/metrics.java
+++ app/src/main/java/com/example/testercapstone/metrics.java
@@ -1,8 +1,13 @@
 package com.example.testercapstone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
 public class metrics extends AppCompatActivity {
@@ -11,5 +16,47 @@ public class metrics extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_metrics);
+
+        // Initialize and assign variable
+        BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
+
+        // Set Welcome selected
+        bottomNavigationView.setSelectedItemId(R.id.metrics);
+
+        // Perform item selected listener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.welcome)
+                {
+                    startActivity(new Intent(getApplicationContext(),UserProfile.class));
+                    overridePendingTransition(0,0);
+                    return true;
+                }
+                else if(item.getItemId() == R.id.userProfile)
+                {
+                    startActivity(new Intent(getApplicationContext(),UserProfile.class));
+                    overridePendingTransition(0,0);
+                    return true;
+                }
+                else if(item.getItemId() == R.id.recordingPage)
+                {
+                    startActivity(new Intent(getApplicationContext(),UserProfile.class));
+                    overridePendingTransition(0,0);
+                    return true;
+                }
+                else if(item.getItemId() == R.id.metrics)
+                {
+                    return true;
+                }
+                else if(item.getItemId() == R.id.resourcesPage)
+                {
+                    startActivity(new Intent(getApplicationContext(),UserProfile.class));
+                    overridePendingTransition(0,0);
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 }
