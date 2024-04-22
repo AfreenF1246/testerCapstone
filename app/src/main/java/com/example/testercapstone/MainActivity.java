0336905413a653aea385b58d98f0d19ba79bea04
AfreenF1246 <af894@rutgers.edu>
2024-04-22 10:08:51 -0400

    forget about ur boyfriend and meet me at the hotel room

--- app/src/main/java/com/example/testercapstone/MainActivity.java
+++ app/src/main/java/com/example/testercapstone/MainActivity.java
@@ -29,8 +29,8 @@ public class MainActivity extends AppCompatActivity {
     EditText username;
     EditText password;
     Button loginButton;
-
     Button signupText;
+    DBHelper DB;
 
     // howdyyy yyy
     @Override
@@ -41,9 +41,29 @@ public class MainActivity extends AppCompatActivity {
         password = findViewById(R.id.password);
         loginButton = findViewById(R.id.loginButton);
         signupText = findViewById(R.id.signupText);
+
+        DB = new DBHelper(this);
+
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+                if(user.equals("")||pass.equals(""))
+                    Toast.makeText(MainActivity.this, "Please enter all the fields", Toast.LENGTH_SHORT).show();
+                else{
+                    Boolean checkuserpass = DB.checkusernamepassword(user, pass);
+                    if(checkuserpass==true){
+                        Toast.makeText(MainActivity.this, "Sign in successful", Toast.LENGTH_SHORT).show();
+                        Intent intent  = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent);
+                    }else{
+                        Toast.makeText(MainActivity.this, "Invalid Credentials", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                /*
                 if (username.getText().toString().equals("user") && password.getText().toString().equals("1234")) {
                     Toast.makeText(MainActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
                     Intent intent = new Intent(MainActivity.this, WelcomePage.class);
@@ -51,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     Toast.makeText(MainActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
 
-                }
+                }*/
             }
         });
 
